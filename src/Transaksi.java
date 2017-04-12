56aaceb6d46111c4224d2b37fd0d7f2ca36c02cb
Fakhri Abdullah <fakhri.abdullah.9496@gmail.com>
2017-04-12 15:43:33 +0700

    transaksi

--- src/Transaksi.java
+++ src/Transaksi.java
@@ -2,6 +2,22 @@
  * Created by Handoko on 4/11/2017.
  */
 public class Transaksi {
+    int id;
+    String tgl;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTgl() {
+        return tgl;
+    }
+
+    public void setTgl(String tgl) {
+        this.tgl = tgl;
+    }
 }

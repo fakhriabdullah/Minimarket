e7f4455c7e9538c7c7220d44370212c5f56b704b
DESKTOP-RHI01IQ\Handoko <handoko.ben@gmail.com>
2017-05-02 22:22:32 +0700

    penjualan dan transaksi penjualan (rev2)

--- src/TransaksiPenjualan.java
+++ src/TransaksiPenjualan.java
@@ -21,7 +21,7 @@ public class TransaksiPenjualan {
     public int hitungTotalPenjualan() {
         int totalPenjualan = 0;
         for (Penjualan p : arrPenjualan) {
-            totalPenjualan = p.jumlah * p.hargaJual;
+            totalPenjualan = totalPenjualan + (p.jumlah * p.hargaJual);
         }
         return totalPenjualan;
     }
@@ -43,11 +43,14 @@ public class TransaksiPenjualan {
     public static void main(String[] args) {
         Karyawan kasir = new Karyawan(1, "Budi", "Gerlong", "0888811212");
         TransaksiPenjualan transP = new TransaksiPenjualan(kasir);
+        Penjualan p = new Penjualan("Rinso", 25000);
         transP.setTunai(50000);
         transP.waktuPembelian = "08 April 2017";
+        p.setJumlah(1);
+        transP.addPenjualan(p);
         System.out.println("Nama Karyawan : "+kasir.namaKaryawan);
         System.out.println("Total Penjualan : "+transP.hitungTotalPenjualan());
-        System.out.println("Nama Karyawan : "+kasir.namaKaryawan);
-
+        System.out.println("Tunai : "+transP.tunai);
+        System.out.println("Kembalian : "+transP.hitungKembalianPenjualan());
     }
 }
